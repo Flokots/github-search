9cb8e1cac917509435448eac0981e6d1dc48bac9
FloKots <florencekotohoyoh@gmail.com>
2022-04-18 14:57:51 +0300

    add landing page, search form components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
