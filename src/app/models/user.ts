5cc7ba2d44595ce937e2850457688c2aeab0ff57
FloKots <florencekotohoyoh@gmail.com>
2022-04-18 23:34:19 +0300

    create a User class

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+  constructor(
+    public name: string,
+    public login: string,
+    public avatar_url: string,
+    public followers: number,
+    public created_at: Date,
+    public updated_at: Date,
+    public public_repos: number
+  ) {}
+}
