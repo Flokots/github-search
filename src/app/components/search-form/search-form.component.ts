df05b8ecab658776d1173f38a8fdb26a2badb352
FloKots <florencekotohoyoh@gmail.com>
2022-04-20 05:35:52 +0300

    add two-way databinding

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -9,12 +9,8 @@ import { SearchInfo } from 'src/app/models/searchInfo/search-info';
 })
 export class SearchFormComponent implements OnInit {
 
-  searchName = new SearchInfo('');
-  @Output() searchUserRepo = new EventEmitter<SearchInfo>();
+  searchName = "";
 
-  submitSearch() {
-    this.searchUserRepo.emit(this.searchName);
-  }
   constructor() { }
 
   ngOnInit(): void {
